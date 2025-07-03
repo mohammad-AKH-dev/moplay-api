9fdcae200d825c763b7540fa10e79647e3d1e0fc
Mohammad (FrontEnd Developer) <mohammadakhlaghi843@gmail.com>
2025-07-03 18:35:00 +0330

    changed route for db.json

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('db.json'); // مسیر db.json نسبت به ریشه پروژه
+const router = jsonServer.router('./backend/db.json'); // مسیر db.json نسبت به ریشه پروژه
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 4000; // پورت 4000
 
